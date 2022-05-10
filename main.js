7860332d643780e61c0a4a03f2d7cf42de3a4cd8
Your Name <you@example.com>
2022-05-10 19:42:36 +0300

    aa

---
+++ main.js
@@ -0,0 +1,49 @@
+function Player(id, name) {
+  this.id = id;
+  this.name = name;
+  this.cards = [];
+  this.active = false;
+  this.addScore = function () {
+    this.pairs++;
+  };
+
+  this.myTurn = function () {
+    document.getElementById(`player${id}`).classList.add("activePlayer");
+  };
+}
+
+function Card(row, col, frontImg) {
+  this.row = row;
+  this.col = col;
+  this.frontImg = frontImg;
+  this.disable = function () {
+    this.divElement.removeEventListener("click", flipCard);
+    this.divElement.style.pointerEvents = "none";
+  };
+}
+
+const board = [[], [], []];
+const players = []
+const card = [];
+const numOfCards = 9;
+let activePlayer = null;
+
+function init() {
+  for (let i = 0; i < numOfCards / 3; i++) {
+
+    for (let j = 0; j < numOfCards / 3; j++) {
+      let card = new Card(i, j, null);
+      board[i][j] = card;
+      let divElement = document.createElement('div');
+      divElement.id = i + "" + j;
+      divElement.classList.add('card');
+      document.querySelector('#board').appendChild(divElement);
+      document.querySelector('#board').classList.add('board');
+    }
+  }
+
+}
+init();
+
+
+
